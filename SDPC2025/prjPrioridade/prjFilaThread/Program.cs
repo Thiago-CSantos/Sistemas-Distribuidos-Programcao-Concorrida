0f85e36de4c3200eb97d091f5840684e1c8627ff
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2025-08-14 20:56:59 -0300

    Add files via upload

---
+++ SDPC2025/prjPrioridade/prjFilaThread/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace prjFilaThread
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Inicio da execução do programa[{Thread.CurrentThread.ManagedThreadId}]");
+            // Execução de um processo em fila de Threads
+
+            GerenciaFila();
+
+            // Sinaliza o fim da execução
+
+            Console.WriteLine($"Fim da execução do programa[{Thread.CurrentThread.ManagedThreadId}]");
+
+        }
+
+        private static void GerenciaFila()
+        {
+            ThreadPool.QueueUserWorkItem(new WaitCallback(ImprimeNumeros));
+
+        }
+
+        private static void ImprimeNumeros(object state)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine($"{i} [{Thread.CurrentThread.ManagedThreadId}]\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SDPC2025/prjPrioridade/prjPrioridade/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace prjPrioridade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // criar 3 THreads para o mesmo precesso com prioridades diferentes
+
+            Thread t1 = new Thread(Processo);
+            Thread t2 = new Thread(Processo);
+            Thread t3 = new Thread(Processo);
+
+            // Personalizando dados de controle das Threads
+            t1.Name = "T1A alta";
+            t2.Name = "T2B normal";
+            t3.Name = "T3C baixo";
+
+            t1.Priority = ThreadPriority.Highest; // prioridade alta
+            t3.Priority = ThreadPriority.Lowest;  // prioridade normal
+
+            // Dados partida das Threads
+            t1.Start();
+            t2.Start();
+            t3.Start();
+
+            Console.WriteLine($"Prioridade de t1 é {t1.Priority} [{t1.Name}] ");
+            Console.WriteLine($"Prioridade de t2 é {t2.Priority} [{t2.Name}] ");
+            Console.WriteLine($"Prioridade de t3 é {t3.Priority} [{t3.Name}] ");
+
+        }
+
+        private static void Processo()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Console.WriteLine($"{i} Thread {Thread.CurrentThread.Name} - {i}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
