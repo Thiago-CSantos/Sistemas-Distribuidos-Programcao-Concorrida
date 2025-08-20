11e43df9c084d92c21e56e08da82f4abd03b6a5a
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2025-08-19 22:41:01 -0300

    Add files via upload

---
+++ SDPC2025/prjFilaThread/prjCronoFluxo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+
+namespace prjCronoFluxo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch crono = new Stopwatch();
+
+            Console.WriteLine("Tradicional>>>>>");
+            crono.Start();
+            FluxoTradicional();
+            crono.Stop();
+
+            Console.WriteLine($"Tempo de consumo é: {crono.ElapsedTicks.ToString()}");
+            crono.Reset();
+
+            Console.WriteLine("Fila Threads >>>>>");
+            crono.Start();
+            FluxoFila();
+            crono.Stop();
+            Console.WriteLine($"Tempo de consumo é: {crono.ElapsedTicks.ToString()}");
+
+        }
+
+        // Thread tradicional
+        private static void FluxoTradicional()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+
+                Thread fluxo = new Thread(GeraNumero);
+                fluxo.Start();
+
+            }
+        }
+
+        // FIla Threads
+        private static void FluxoFila()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(GeraNumero));
+            }
+        }
+
+        // Processo a ser executado
+        private static void GeraNumero(object estado)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{i} ID da Thread[{Thread.CurrentThread.ManagedThreadId}]");
+            }
+        }
+
+    }
+}
---
+++ SDPC2025/prjFilaThread/prjLojaOnline/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace prjLojaOnline
+{
+
+    class Pedido
+    {
+        public int ID {  get; set; }
+        public String Cliente { get; set; }
+        public decimal valor {  get; set; }
+
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Pedido> pedido = new List<Pedido>()
+            {
+                new Pedido {ID = 1, Cliente = "Ana", valor = 450.00m},
+                new Pedido {ID = 2, Cliente = "Bruno", valor = 150.00m},
+                new Pedido {ID = 3, Cliente = "Carla", valor = 99.00m},
+            };
+
+            foreach (var item in pedido)
+            {
+                ThreadPool.QueueUserWorkItem(ProcessaPedido, item);
+            }
+            Console.WriteLine("Aguardando processamento...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Todos os pedidos foram processados");
+
+
+            private static void ProcessaPedido(object estado)
+        {
+
+        }
+        }
+    }
+}
