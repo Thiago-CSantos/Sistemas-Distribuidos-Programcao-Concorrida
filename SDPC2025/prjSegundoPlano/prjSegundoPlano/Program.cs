7e77e33b8901f9f64940dc1e695c616db614e5d1
Thiago C. dos Santos <104088057+Thiago-CSantos@users.noreply.github.com>
2025-08-21 20:59:32 -0300

    Add files via upload

---
+++ SDPC2025/prjSegundoPlano/prjSegundoPlano/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.ComponentModel;
+
+namespace prjSegundoPlano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var segPlano = new BackgroundWorker();
+
+            segPlano.WorkerReportsProgress = true;
+            segPlano.WorkerSupportsCancellation = true;
+
+            segPlano.DoWork += servicoPessoal;
+            segPlano.ProgressChanged += progresso;
+            segPlano.RunWorkerCompleted += processoCompleto;
+            segPlano.RunWorkerAsync();
+            Console.WriteLine("C canela o processo");
+
+            while (segPlano.IsBusy)
+            {
+                if (Console.ReadKey(true).KeyChar == 'C')
+                {
+                    segPlano.CancelAsync();
+                }
+            }
+            Console.WriteLine(">>> FIM");
+        }
+
+        // Processo para execução em Segundo Plano
+        private static void servicoPessoal(Object? sender, DoWorkEventArgs e)
+        {
+
+        }
+
+        private static void processoCompleto(object? sender, RunWorkerCompletedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void progresso(object? sender, ProgressChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
